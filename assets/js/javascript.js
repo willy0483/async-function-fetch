b40ae8132b4047fa5255a1f10465f3e9c93c3403
willy0483 <willyjensen251@gmail.com>
2024-09-03 11:54:33 +0200

    -

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,11 +4,10 @@ function showDog() {
   fetch("https://dog.ceo/api/breeds/image/random")
     .then((response) => {
       //   console.log(response);
-
       return response.json();
     })
     .then((data) => {
-      //   console.log(data.message);
+      //   console.table(data);
 
       let myDog = document.getElementById("myDogSection");
       myDog.innerHTML = `<img src="${data.message}">`;
